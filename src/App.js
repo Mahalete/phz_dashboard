dbf58cef07628867ca10a868b8804d3c5906cb17
BC-macair011 <mahialemu@gmail.com>
2022-05-04 12:55:57 +0300

    fix date setter to change with chart

--- src/App.js
+++ src/App.js
@@ -9,96 +9,10 @@ import IntegrationPage from "./Components/IntegrationPage-Content/IntegrationPag
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Routes, Route } from "react-router-dom";
-
-import styles from "../src/Components/SideMenu.module.css";
-import { SideMenuData } from "../src/Components/SideMenuData";
-import { ReactComponent as Logo } from "../src/assets/PHZ _ ProScore.svg";
-import { Link, useLocation } from "react-router-dom";
-
-import headerStyle from "../src/Components/Header.module.css";
-import avatar from "../src/assets/unsplash_ILip77SbmOE.png";
-import "react-datepicker/dist/react-datepicker.css";
-import DatePicker from "react-datepicker";
 import moment from "moment";
 
-export const Header = () => {
-  const [loadingDaterange, setLoadingDaterange] = useState(true);
-  const [npsdatawithdaterange, setNpsdatawithdaterange] = useState([]);
-  const [startDate, setStartDate] = useState(
-    new Date().setMonth(new Date().getMonth() - 6)
-  );
-  const [endDate, setEndDate] = useState(new Date());
-  let location = useLocation();
-  return (
-    <>
-      <div className={headerStyle.header}>
-        {SideMenuData.map((val, key) => {
-          if (location.pathname === val.link) {
-            return (
-              <div key={key}>
-                <p className={headerStyle.title}>{val.title}</p>
-              </div>
-            );
-          }
-          return null;
-        })}
-        <div className={headerStyle.datePickerContainer}>
-          <div className={headerStyle.datePicker}>
-            <p className={headerStyle.startTitle}>Start_Date: </p>
-            <DatePicker
-              wrapperClassName={headerStyle.startDatePicker}
-              dateFormat="yyyy-MM-dd"
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-            />
-
-            <p className={headerStyle.endTitle}> End_Date: </p>
-            <br></br>
-            <DatePicker
-              className={headerStyle.endDatePicker}
-              dateFormat="yyyy-MM-dd"
-              selected={endDate}
-              onChange={(date) => setEndDate(date)}
-            />
-          </div>
-        </div>
-
-        <div className={headerStyle.titleRight}>
-          <img className={headerStyle.avatar} src={avatar} alt="avatar" />
-          <div className={headerStyle.userName}>
-            <p className={headerStyle.fullName}>Antti Hätinen</p>
-            <p className={headerStyle.ocupation}>CEO</p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-
-export const SideMenu = () => {
-  let location = useLocation();
-  return (
-    <div data-testid="menu" className={styles.side_menu}>
-      <Logo className={styles.logo} data-testid="company-logo" />
-      <ul className={styles.side_menu_list}>
-        {SideMenuData.map((val, key) => {
-          return (
-            <Link key={key} to={val.link}>
-              <li
-                id={location.pathname === val.link ? styles.active : ""}
-                className={styles.row}
-              >
-                {" "}
-                <div className={styles.icon}>{val.icon}</div>{" "}
-                <div className={styles.title}>{val.title}</div>
-              </li>
-            </Link>
-          );
-        })}
-      </ul>
-    </div>
-  );
-};
+import Header from "./Components/Header";
+import { SideMenu } from "./SideMenu";
 
 function App() {
   const [npsdata, setNpsdata] = useState([]);
@@ -130,6 +44,14 @@ function App() {
   };
   console.log("npsdata ", npsdata);
 
+  const setDates = (setter, date) => {
+    if (setter === "startDate") {
+      setStartDate(date);
+    } else {
+      setEndDate(date);
+    }
+  };
+
   useEffect(() => {
     getNpsdata();
     const getNpsdataWithDateRange = () => {
@@ -152,7 +74,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Header dateGiver={setDates} />
       <SideMenu />
       <Routes>
         <Route

b0e96a533cc853eb46e9f85b5f6266c08d9e3945
BC-macair011 <mahialemu@gmail.com>
2022-05-17 13:04:30 +0300

    Dashboard whenempty show nodata

--- src/Components/Dashbord-Content/Dashboard.js
+++ src/Components/Dashbord-Content/Dashboard.js
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NpsCharts from "./Charts/NpsCharts";
 import NpsSurveyStatus from "./Survey_Status/NpsSurveyStatus";
 import RecentComments from "./Comments/RecentComments";
+import Styles from "./Dashboard.module.css";
 import { FILTERS } from "../CommentPage-Content/CommentPage";
 
 const Dashboard = ({ data, dateRangeData }) => {
+  useEffect(() => {
+    setFilter(dateRangeData);
+  }, [dateRangeData]);
+  console.log("Dashboard ", data.length, dateRangeData.length);
+
   const [filter, setFilter] = useState(FILTERS.ALL);
 
   return (
     <div>
-      {dateRangeData.length > 0 && (
+      {!dateRangeData ||
+        (dateRangeData.length === 0 && (
+          <p className={Styles.feedback_empty}>No data found.</p>
+        ))}
+      {dateRangeData && dateRangeData.length > 0 && (
         <NpsCharts setFilter={setFilter} data={dateRangeData} />
       )}
 
-      {dateRangeData.length > 0 && (
+      {dateRangeData && dateRangeData.length > 0 && (
         <RecentComments filter={filter} data={dateRangeData} />
       )}
 
-      {data && <NpsSurveyStatus npsdata={data} />}
+      {data && data.length > 0 && <NpsSurveyStatus npsdata={data} />}
     </div>
   );
 };

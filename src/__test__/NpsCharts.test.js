292fe0cf5420457bce19a7d63610749d2a53d1e6
BC-macair011 <mahialemu@gmail.com>
2022-05-20 13:24:21 +0300

    All Dashboard tests now pass

--- src/__test__/NpsCharts.test.js
+++ src/__test__/NpsCharts.test.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import NpsCharts from "../Components/Dashbord-Content/Charts/NpsCharts";
 
-const testData = [({ Promoters: 10 }, { Detractors: 5 }, { Neutral: 2 })];
+//const testData = [({ Promoters: 10 }, { Detractors: 5 }, { Neutral: 2 })];
 // jest.mock("react-minimal-pie-chart", () => ({
 //   Doughnut: () => null,
 // }));
 
 test("render the chart", () => {
-  jest.mock("react-minimal-pie-chart", (testData) => ({
+  jest.mock("react-minimal-pie-chart", () => ({
     NPS_Charts: (testData) => null,
   }));
 });
--- src/index.js
+++ src/index.js
@@ -7,40 +7,6 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <App />,
-    <body id="body" style={{ height: "unset" }}>
-      <iframe
-        id="iframe"
-        style={{
-          height: "100vh",
-          width: "100vw",
-          position: "absolute",
-          top: "0px",
-          Zindex: "100",
-          borderRadius: "10px",
-        }}
-        title="survey"
-        src="http://localhost:3001/survey"
-      ></iframe>
-      <script>
-        {window.addEventListener("message", (ev) => {
-          let iFrame = document.getElementById("iframe");
-          let body = document.getElementById("body");
-          if (ev.origin === "http://localhost:3001") {
-            body.style.setProperty("height", ev.data.message.bodyHeight);
-            body.style.setProperty("position", ev.data.message.bodyPosition);
-            body.style.setProperty("top", ev.data.message.bodyTop);
-            body.style.setProperty("left", ev.data.message.bodyLeft);
-            iFrame.style.setProperty("height", ev.data.message.iFrameHeight);
-            iFrame.style.setProperty("width", ev.data.message.width);
-            iFrame.style.setProperty("top", ev.data.message.top);
-            iFrame.style.setProperty("left", ev.data.message.left);
-            iFrame.style.setProperty("bottom", ev.data.message.bottom);
-            iFrame.style.setProperty("display", ev.data.message.display);
-            iFrame.style.setProperty("border", ev.data.message.border);
-          }
-        })}
-      </script>
-    </body>
+    <App />
   </BrowserRouter>
 );
